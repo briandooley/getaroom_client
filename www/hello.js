7ba3d3b032993802028a3c53cf55f77dc455a097
support@example.com <support@example.com>
2016-09-16 11:13:51 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -11,13 +11,13 @@ $fh.cloud(
       for (i = 0; i < res.length; i++) {
         switch(res[i].rank) {
         case 0:
-            style = '<p style="background-color:#fb122f;">';
+            style = '<p style="background-color:#fb122f;font-size:300%;">';
             break;
         case 9999:
-            style = '<p style="background-color:#27e833;">';
+            style = '<p style="background-color:#27e833;font-size:300%;">';
             break;
         default:
-            style = '<p style="background-color:#fad201;">';
+            style = '<p style="background-color:#fad201;font-size:300%;">';
         }
 
         output = output + style + res[i].room + ' - ' + res[i].text + '</p>';
