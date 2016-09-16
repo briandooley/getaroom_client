99c6ca7dc97b23ba56f8500fae97bdcbfa8cb34d
support@example.com <support@example.com>
2016-09-16 11:04:18 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -11,22 +11,20 @@ $fh.cloud(
       for (i = 0; i < res.length; i++) {
         //console.log(res[i].room + ' - ' + res[i].text);
 
-/*
         switch(res[i].rank) {
         case 0:
-            style = '<p style="background-color:red;>';
+            style = '<p style="background-color:red;">';
             break;
         case 9999:
-            style = '<p style="background-color:yellow;>';
+            style = '<p style="background-color:yellow;">';
             break;
         default:
-            style = '<p style="background-color:green;>';
+            style = '<p style="background-color:green;">';
 
         }
-*/
 
-//        output = output + style + res[i].room + ' - ' + res[i].text + '</p>';
-        output = output + '<p style="background-color:red;">' + res[i].room + ' - ' + res[i].text + '</p>';
+//        output = output + '<p style="background-color:red;">' + res[i].room + ' - ' + res[i].text + '</p>';
+        output = output + style + res[i].room + ' - ' + res[i].text + '</p>';
         console.log(output);
       }
       console.log(output);
