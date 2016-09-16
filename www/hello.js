3467fc2c702f44996be121052c100dd232218ab8
support@example.com <support@example.com>
2016-09-16 10:00:44 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -17,28 +17,3 @@ $fh.cloud(
       alert('An error occured: ' + code + ' : ' + errorprops);
     }
 );
-
-/*
-document.getElementById('say_hello').onclick = function () {
-  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
-  $fh.cloud(
-      {
-        path: 'hello',
-        method: 'GET',
-        contentType: 'application/json'
-      },
-      function (res) {
-        console.log(res);
-        var output = '<p>';
-        for (i = 0; i < res.length; i++) {
-          console.log(res[i].room + ' - ' + res[i].text);
-          output = output + res[i].room + ' - ' + res[i].text + '<p>';
-        }
-        document.getElementById('cloudResponse').innerHTML = output;
-      },
-      function (code, errorprops, params) {
-        alert('An error occured: ' + code + ' : ' + errorprops);
-      }
-  );
-};
-*/
