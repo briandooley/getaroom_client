65f2c493b6fe6f3caca4908fae75261eb7ab8aea
support@example.com <support@example.com>
2016-09-16 10:20:37 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -6,7 +6,7 @@ $fh.cloud(
     },
     function (res) {
       console.log(res);
-      var output = 'style="background-color:powderblue;"<p>';
+      var output = '<p><style="background-color:powderblue;">';
       for (i = 0; i < res.length; i++) {
         console.log(res[i].room + ' - ' + res[i].text);
         output = output + res[i].room + ' - ' + res[i].text + '<p>';
