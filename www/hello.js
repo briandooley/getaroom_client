e1f089cbb4864a49ea324d678aebd80421ca5b97
support@example.com <support@example.com>
2016-09-16 10:32:36 +0000

    update file /www/hello.js

--- www/hello.js
+++ www/hello.js
@@ -6,9 +6,21 @@ $fh.cloud(
     },
     function (res) {
       console.log(res);
+      var style = '';
       var output = '<p style="background-color:powderblue;">';
       for (i = 0; i < res.length; i++) {
         console.log(res[i].room + ' - ' + res[i].text);
+        switch(res[i].rank) {
+        case 0:
+            style = '<p style="background-color:red;>';
+            break;
+        case 9999:
+            style = '<p style="background-color:yellow;>';
+            break;
+        default:
+            default: style = '<p style="background-color:green;>';
+
+        }
         output = output + res[i].room + ' - ' + res[i].text + '<p>';
       }
       document.getElementById('cloudResponse').innerHTML = output;
